e458ed0d7a671b9b3c04b558cd28f2bb1b02b30f
mariiarechkova <mariia.rechkova@gmail.com>
2025-02-19 17:50:05 +0100

    SP-29: get an updated list of ratings based on the normalization factor

--- voting/services/calculation_services/normalization_service.py
+++ voting/services/calculation_services/normalization_service.py
@@ -3,21 +3,60 @@ from voting.models import VoteRoundDetails
 
 class NormalizationService:
     def __init__(self):
-        self.estimations = EstimationsRepository.get_judges_with_estimations()
+        self.judge_estimations = JudgeEstimationsRepository.get_judges_with_estimations()
+        self.rated_estimations = RatedUserEstimationsRepository.get_rated_users_with_estimations()
 
     def calculate_normalization_coefficient(self):
-        if not self.estimations:
+        if not self.judge_estimations:
             raise ValueError("The estimations data is empty.")
         normalization_coefficients = {}
 
-        for judge_id, estimations in self.estimations.items():
+        for judge_id, estimations in self.judge_estimations.items():
             normalization_coefficients[judge_id] = round(((len(estimations) + 1) * 5) / (sum(estimations) + 5), 4)
 
         return normalization_coefficients
 
-class EstimationsRepository:
+    def calculate_updated_estimations(self):
+        normalization_coefficients = self.calculate_normalization_coefficient()
+        rated_estimations = self.rated_estimations
+        for key in normalization_coefficients.keys():
+            for rated_user, row in rated_estimations.items():
+                if key == rated_user and key not in row.keys():
+                    row[key] = 5.0
+
+
+        updated_estimations = {}
+
+        for rated_user, judges in rated_estimations.items():
+            updated_estimations[rated_user] = {}
+            for key in judges.keys() & normalization_coefficients.keys():
+                updated_estimations[rated_user][key] = round((judges[key] * normalization_coefficients[key]),2)
+
+        return updated_estimations
+
+class JudgeEstimationsRepository:
     @staticmethod
     def get_judges_with_estimations():
         estimations_list = VoteRoundDetails.objects.values('judge__id').annotate(estimation = ArrayAgg('estimation'))
         estimations = {item['judge__id']: item['estimation'] for item in estimations_list}
         return estimations
+
+class RatedUserEstimationsRepository:
+    @staticmethod
+    def get_rated_users_with_estimations():
+        estimations_list = (VoteRoundDetails.objects.values('rated_user__id', 'judge__id', 'estimation').
+                            order_by('rated_user__id', 'judge__id'))
+
+        rated_user_estimations = {}
+        for item in estimations_list:
+            rated_user_id = item['rated_user__id']
+            judge_id = item['judge__id']
+            estimation = item['estimation']
+
+            # Grouping by rated_user_id
+            if rated_user_id not in rated_user_estimations:
+                rated_user_estimations[rated_user_id] = {}
+
+            rated_user_estimations[rated_user_id][judge_id] = estimation
+
+        return rated_user_estimations
--- voting/tests/test_normalization_service.py
+++ voting/tests/test_normalization_service.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 from organisations.models import Organisation
 from users.models import User
 from voting.models import VoteEvent, VoteRound, VoteRoundDetails
-from voting.services.calculation_services.normalization_service import NormalizationService, EstimationsRepository
+from voting.services.calculation_services.normalization_service import (NormalizationService, JudgeEstimationsRepository,
+                                                                        RatedUserEstimationsRepository)
 from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
@@ -34,6 +35,7 @@ class CalculationNormalizationTestCase(TestCase):
 
         for i, (first_name, last_name, email) in enumerate(user_data, start=1):
             setattr(self, f"user{i}", User.objects.create(
+                id = i,
                 first_name=first_name,
                 last_name=last_name,
                 email=email,
@@ -66,15 +68,15 @@ class CalculationNormalizationTestCase(TestCase):
         VoteRoundDetails.objects.bulk_create(votes)
 
     def test_get_judges_with_estimations(self):
-        result = EstimationsRepository.get_judges_with_estimations()
+        result = JudgeEstimationsRepository.get_judges_with_estimations()
 
         expected_result = {
-            self.user1.id: [6, 5, 5, 6],
-            self.user2.id: [6, 4, 4, 5, 4],
-            self.user3.id: [5, 7, 5, 6],
-            self.user4.id: [4, 5, 5, 5, 5],
-            self.user5.id: [4, 5, 4, 4, 6],
-            self.user6.id: [5, 5, 5, 5, 6]
+            1: [6, 5, 5, 6],
+            2: [6, 4, 4, 5, 4],
+            3: [5, 7, 5, 6],
+            4: [4, 5, 5, 5, 5],
+            5: [4, 5, 4, 4, 6],
+            6: [5, 5, 5, 5, 6]
         }
         self.assertEqual(result, expected_result)
 
@@ -85,12 +87,12 @@ class CalculationNormalizationTestCase(TestCase):
         result = normalization_service.calculate_normalization_coefficient()
 
         expected_coefficient = {
-            self.user1.id: 0.9259,
-            self.user2.id: 1.0714,
-            self.user3.id: 0.8929,
-            self.user4.id: 1.0345,
-            self.user5.id: 1.0714,
-            self.user6.id: 0.9677
+            1: 0.9259,
+            2: 1.0714,
+            3: 0.8929,
+            4: 1.0345,
+            5: 1.0714,
+            6: 0.9677
         }
 
         self.assertEqual(result, expected_coefficient)
@@ -104,3 +106,32 @@ class CalculationNormalizationTestCase(TestCase):
             normalization_service.calculate_normalization_coefficient()
 
         self.assertEqual(str(context.exception), "The estimations data is empty.")
+
+    def test_get_rated_users_with_estimations(self):
+        result = RatedUserEstimationsRepository.get_rated_users_with_estimations()
+
+        expected_result = {
+            1: {2: 6.0, 3: 5.0, 4: 4.0, 5: 4.0, 6: 5.0},
+            2: {1: 6.0, 3: 7.0, 4: 5.0, 5: 5.0, 6: 5.0},
+            3: {1: 5.0, 2: 4.0, 4: 5.0, 5: 4.0, 6: 5.0},
+            4: {1: 5.0, 2: 4.0, 3: 5.0, 5: 4.0, 6: 5.0},
+            5: {1: 6.0, 2: 5.0, 3: 6.0, 4: 5.0, 6: 6.0},
+            6: {2: 4.0, 4: 5.0, 5: 6.0},
+        }
+        self.assertEqual(result, expected_result)
+
+    def test_calculate_updated_estimations(self):
+        normalization_service = NormalizationService()
+        result = normalization_service.calculate_updated_estimations()
+        expected_result = {
+            1: {1: 4.63, 2: 6.43, 3: 4.46, 4: 4.14, 5: 4.29, 6: 4.84},
+            2: {1: 5.56, 2: 5.36, 3: 6.25, 4: 5.17, 5: 5.36, 6: 4.84},
+            3: {1: 4.63, 2: 4.29, 3: 4.46, 4: 5.17, 5: 4.29, 6: 4.84},
+            4: {1: 4.63, 2: 4.29, 3: 4.46, 4: 5.17, 5: 4.29, 6: 4.84},
+            5: {1: 5.56, 2: 5.36, 3: 5.36, 4: 5.17, 5: 5.36, 6: 5.81},
+            6: {2: 4.29, 4: 5.17, 5: 6.43, 6: 4.84},
+        }
+
+
+
+        self.assertEqual(result, expected_result)
